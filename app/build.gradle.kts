4f0a92bc5c913060c8a119c2315a4836ce87b3c3
Inonvation <197215650+Inonvation@users.noreply.github.com>
2026-09-21 19:42:29 +0800

    chore: 版本号 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "edu.jxslu.schedule"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.0"
+        versionCode = 5
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
